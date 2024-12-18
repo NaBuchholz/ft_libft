186b087dd42bd968784e681e616b34694542790b
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2024-12-18 15:30:15 -0300

    fix norminette errors

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -1,9 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 15:26:24 by nbuchhol          #+#    #+#             */
+/*   Updated: 2024/12/18 15:26:51 by nbuchhol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	if (!lst|| !f)
+	if (!lst || !f)
 		return ;
 	while (lst)
 	{
@@ -11,5 +22,3 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = lst->next;
 	}
 }
-
-//TODO add 42 header
